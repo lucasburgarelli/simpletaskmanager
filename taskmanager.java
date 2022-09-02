04b7fcb1362d165f1156576d15962b9a9232c8ef
lucasburgarelli <92059288+lucasburgarelli@users.noreply.github.com>
2022-09-02 14:59:48 -0300

    Update taskmanager.java

--- taskmanager.java
+++ taskmanager.java
@@ -3,50 +3,38 @@ import java.util.*;
 
 public class taskmanager {
 	public static void main(String[] args) {
-		String help = System.lineSeparator()+"0 -> list"+System.lineSeparator()+"1 + task -> add"+System.lineSeparator()+"2 + task -> remove";
-		
-		String task = "";
-		for(int i = 1; i< args.length; i++)
-			task += args[i] + " ";
-			
 		try {
-	        Scanner scanner = new Scanner(new File("todo.txt"));
+			Scanner scanner = new Scanner(new File("todo.txt"));
 	        
-	        if(args[0].equals("0")) {
-	        	while(scanner.hasNextLine())
-					System.out.println(scanner.nextLine());
-	        	
-				return;
-	        }
-	        
-	        if(args[0].equals("1")) {
-	        	Writer output = new BufferedWriter(new FileWriter("todo.txt", true));
-				output.append(task + System.lineSeparator());
-				output.close();
-				return;
-	        }
-	        
-	        if(args[0].equals("2")) {
-				ArrayList<String> updatedList = new ArrayList<String>();
-				
-				while(scanner.hasNextLine()) {
-					String line = scanner.nextLine();
-					
-					if(!line.toLowerCase().equals(task.toLowerCase()))
-						updatedList.add(line);
-				}
-				
-				Writer output = new BufferedWriter(new FileWriter("todo.txt"));
-				for (String line : updatedList) 
+			List<String> tasks = new ArrayList<String>();
+
+				while(scanner.hasNextLine())
+					tasks.add(scanner.nextLine());
+
+			StringBuilder taskBuilder = new StringBuilder();
+
+			for(int i = 1; i< args.length; i++) 
+				taskBuilder.append(args[i] + " ");
+
+			String taskInput = taskBuilder.toString();
+
+
+			if(args[0].equals("-add") || args[0].equals("-a"))
+				tasks.add(taskInput.toString());
+
+			if(args[0].equals("-remove") || args[0].equals("-r"))
+				tasks.removeIf(task -> task.toLowerCase().equals(taskInput.toLowerCase()));
+
+
+			Writer output = new BufferedWriter(new FileWriter("todo.txt"));
+
+				for (String line : tasks)
 					output.write(line + System.lineSeparator());
-				output.close();
-				
-				return;
-	        }
-	        
-	        System.out.print("Unknown command" + help);
+			tasks.forEach(task -> System.out.println(task));
+
+			output.close();
 		}catch(Exception e){
-			System.out.print("Error" + help);
+			System.out.println("Options: -list ; -add newTask; -remove newTask");
 		}
 	}
 }
